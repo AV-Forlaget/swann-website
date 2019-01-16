4cb9a5014829257ea3b03b5ea06df1de3750f5b3
Frederik Stahlfest Møller <frederik@edentic.dk>
2019-01-16 13:48:34 +0100

    Setup docker on project

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,5 +1,9 @@
 <template>
   <div>
+  <div class="main-header">
+  </div>
     <nuxt/>
+     <div class="main-footer">
+  </div>
   </div>
 </template>
---
+++ run.sh
@@ -0,0 +1,9 @@
+echo "*** Starting DOCKER containers... ***"
+docker-compose build
+docker-compose up -d
+
+echo "*** Updating node dependencies... ***"
+docker-compose exec kluboplevelser_frontend npm install
+
+echo "*** Running Gulp .. ***"
+docker-compose exec kluboplevelser_frontend npm run dev
