0f70f321995d7c76d3e73dad6b9c24b9a0bbaed9
Frederik Stahlfest Møller <frederik@edentic.dk>
2019-01-16 14:34:23 +0100

    Added config, header and footer

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,9 +1,38 @@
 <template>
-  <div>
-  <div class="main-header">
-  </div>
+  <div class="main-app">
+    <div class="main-header">
+      <div class="main-header__wrapper">
+        <div class="main-logo">
+          
+        </div>
+        <div class="main-nav">
+          <ul class="nav-list">
+                <li class="nav-list__item">
+                    <a>Forisde</a>
+                    <a>Oplevelser</a>
+                    <a>Vision</a>
+                    <a>Kontakt</a>
+                </li>
+            </ul>
+        </div>
+        <button>Log ind</button>
+      </div>
+    </div>
+
     <nuxt/>
-     <div class="main-footer">
-  </div>
+
+    <div class="main-footer">
+      <div class="main-footer__wrapper">
+        <div class="footer-col">
+          
+        </div>
+        <div class="footer-col">
+          
+        </div>
+        <div class="footer-col">
+          
+        </div>
+      </div>
+    </div>
   </div>
 </template>
