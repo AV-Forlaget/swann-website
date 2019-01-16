669de1e982bed39620c623d6717198514745b157
Frederik Stahlfest Møller <frederik@edentic.dk>
2019-01-16 20:05:05 +0100

    Styled basic header

--- layouts/default.vue
+++ layouts/default.vue
@@ -3,23 +3,27 @@
     <div class="main-header">
       <div class="main-header__wrapper">
         <div class="main-logo">
-          
+          <img src="@/assets/img/kluboplevelser-logo.svg" alt="">
         </div>
-        <div class="main-nav">
-          <ul class="nav-list">
+        <ul class="nav-list">
                 <li class="nav-list__item">
                     <a>Forisde</a>
+                </li>
+                  <li class="nav-list__item">
                     <a>Oplevelser</a>
+                </li>
+                  <li class="nav-list__item">
                     <a>Vision</a>
+                </li>
+                  <li class="nav-list__item">
                     <a>Kontakt</a>
                 </li>
             </ul>
-        </div>
-        <button>Log ind</button>
+        <button class="btn btn--orange">Log ind</button>
       </div>
     </div>
 
-    <nuxt/>
+    <!--<nuxt/>-->
 
     <div class="main-footer">
       <div class="main-footer__wrapper">
