4ef1f8f3628f19af6f1d4c6f37085128fe049779
Frederik Stahlfest Møller <frederik@edentic.dk>
2019-01-21 11:49:59 +0100

    Added icon mobile login btn

--- layouts/default.vue
+++ layouts/default.vue
@@ -21,6 +21,7 @@
         </ul>
         <button class="btn btn--orange">
           <span>Log ind</span>
+          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 134.9 159.8"><path d="M130.9,155.8s-17.5-49.4-63.4-49.4S4,155.8,4,155.8" style="fill:none;stroke:#fff;stroke-linecap:round;stroke-miterlimit:10;stroke-width:8px"/><ellipse cx="67.5" cy="47.4" rx="37.6" ry="43.4" style="fill:none;stroke:#fff;stroke-linecap:round;stroke-miterlimit:10;stroke-width:8px"/></svg>
         </button>
          <div class="mobile-menu">
                 <div class="mobile-menu__hamburger">
