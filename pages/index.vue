b0cc4adf936e0e4e7a287363e67193a931e9bb08
Frederik Stahlfest Møller <frederik@edentic.dk>
2019-01-21 16:24:35 +0100

    Improved styling on content blocks, and line bg illustrations

--- pages/index.vue
+++ pages/index.vue
@@ -1,25 +1,47 @@
 <template>
   <section class="main-content">
-      <div class="content-section">
-        <div class="content-section__wrapper">
-          <div class="content-block">
-            <h2>.</h2>
-          </div>
-          <div class="content-block">
-          </div>
-        </div>
-      </div>
     <div class="content-section">
-        <div class="content-section__wrapper">
-          <div class="content-block">
-            <h2>Vi gør dine klubdrømme til virkelighed</h2>
-            <p>Når I har en drøm om at opleve noget sammen, er I allerede langt. Der skal selvfølgelig penge til at indfrie den, og det kan vi hjælpe jer med.</p>
-          </div>
-          <div class="content-block">
-              <img src="@/assets/img/sport-objects.svg" alt="" class="sport-objects">
-          </div>
+      <div class="content-section__wrapper">
+        <div class="content-block">
+          <h2>Slides</h2>
+        </div>
+        <div class="content-block">
         </div>
       </div>
+    </div>
+    <div class="content-section content-section--lines">
+      <div class="content-section__wrapper">
+        <div class="content-block">
+          <h2>Vi gør dine klubdrømme til virkelighed</h2>
+          <p>Når I har en drøm om at opleve noget sammen, er I allerede langt. Der skal selvfølgelig penge til at indfrie den, og det kan vi hjælpe jer med.</p>
+          <button class="btn">Tilmeld dig nu</button>
+        </div>
+        <div class="content-block">
+            <img src="@/assets/img/sport-objects.svg" alt="" class="sport-objects">
+        </div>
+      </div>
+    </div>
+    <div class="content-section">
+      <div class="content-section__wrapper">
+        <div class="content-block">
+          <h2>Testimonials</h2>
+        </div>
+        <div class="content-block">
+        </div>
+      </div>
+    </div>
+    <div class="content-section content-section--lines-reversed">
+      <div class="content-section__wrapper">
+        <div class="content-block">
+          <h2>Vi gør dine klubdrømme til virkelighed</h2>
+          <p>Når I har en drøm om at opleve noget sammen, er I allerede langt. Der skal selvfølgelig penge til at indfrie den, og det kan vi hjælpe jer med.</p>
+          <button class="btn">Tilmeld dig nu</button>
+        </div>
+        <div class="content-block">
+            <img src="@/assets/img/sport-objects.svg" alt="" class="sport-objects">
+        </div>
+      </div>
+    </div>
   </section>
 </template>
 
