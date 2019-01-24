78aa30fa1e978afc1ec3c1f4b2a52b9af94de790
Frederik Stahlfest Møller <frederik@edentic.dk>
2019-01-24 11:55:37 +0100

    Added and styled login page

---
+++ pages/login.vue
@@ -0,0 +1,16 @@
+<template>
+  <section class="main-content">
+    <div class="default-page default-page--no-flex default-page--login">
+        <div class="content-section">
+            <div class="content-section__wrapper">
+                <div class="content-block">
+                <h2>Log ind</h2>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Illo, temporibus consequatur ipsa quae autem officiis ea tenetur sed cum, esse a. Quibusdam praesentium optio harum, ab expedita beatae magnam sapiente? <nuxt-link to="/kontakt">Kontakt os</nuxt-link>.</p>
+                <a href="https://morud2.kluboplevelser.dk/web/login?redirect=/shop" class="btn btn--orange">MORUD U14</a>
+                <a href="https://tpi1.kluboplevelser.dk/web/login?redirect=/shop" class="btn btn--orange">TPI</a>
+                </div>
+            </div>
+        </div>
+    </div>
+  </section>
+</template>
