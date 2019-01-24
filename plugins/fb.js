92850ff069321c26c4ae4d5232b01a5559363ce9
Kasper Baagø Jensen <kasper@edentic.dk>
2019-01-24 15:50:11 +0100

    Changed to danish version of plugin

--- plugins/fb.js
+++ plugins/fb.js
@@ -4,7 +4,7 @@ vue_fb.install = function Install(Vue, options) {
         var js, fjs = d.getElementsByTagName(s)[0];
         if (d.getElementById(id)) return;
         js = d.createElement(s); js.id = id;
-        js.src = 'https://connect.facebook.net/en_US/sdk.js#xfbml=1&version=v3.2';
+        js.src = 'https://connect.facebook.net/da_DK/sdk.js#xfbml=1&version=v3.2';
         fjs.parentNode.insertBefore(js, fjs);
       }(document, 'script', 'facebook-jssdk'));
 }
