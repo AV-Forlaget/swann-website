70c563a1c1feb975f3896f155581f241aacf7d01
Frederik Stahlfest Møller <frederik@edentic.dk>
2019-02-13 12:20:19 +0100

    Added basic stylig to terms and confirmation on step 4

--- pages/step-4.vue
+++ pages/step-4.vue
@@ -7,13 +7,18 @@
       <checkbox name="May we display your name and sound sample on the publicly accesible
 part of our website?"></checkbox>
       <input-field name="Salary"></input-field>
-      <a href="#">Terms of service</a>
-      <checkbox name="I have read Swann Studio’s terms of service"></checkbox>
+      </div>
+       <div class="terms-conditions">
+        <a href="#" target="_blank">Terms of service</a>
+        <checkbox name="I have read Swann Studio’s terms of service"></checkbox>
+      </div>
+       <div class="human-confirmation" style="padding:1em; box-sizing:border-box; background:#eee;">
+        <p>I'm not a robot</p>
+      </div>
         <div class="survey-buttons">
             <nuxt-link to="/step-3" class="btn btn--grey">Previous</nuxt-link>
             <nuxt-link to="/done" class="btn btn--green">Send application</nuxt-link>
         </div>
-      </div>
   </section>
 </template>
 
