e7780d1f9f58f444bf41996e82b8784cc01795da
Frederik Stahlfest Møller <frederik@edentic.dk>
2019-02-13 11:55:20 +0100

    Styled dropdowns

--- components/dropdown.vue
+++ components/dropdown.vue
@@ -5,6 +5,7 @@
                 <ul class="suggestion-list">
                     <slot></slot>
                 </ul>
+                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 395.65 223" class="icon-dropdown"><path d="M370.5,0a25,25,0,0,0-17.79,7.37L210.57,149.51l-12.73,12.73-12.73-12.73L43,7.37A25.16,25.16,0,1,0,7.38,43L180,215.63a25.16,25.16,0,0,0,35.58,0L388.29,43A25.17,25.17,0,0,0,370.5,0Z"/></svg>
             </div>
         </div>
 </template>
