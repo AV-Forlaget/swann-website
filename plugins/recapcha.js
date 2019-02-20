5f30fe9d5c05bb7dab66f7d316732d0b44236142
Kasper Baagø Jensen <kasper@edentic.dk>
2019-02-20 21:46:18 +0100

    Removed site key

--- plugins/recapcha.js
+++ plugins/recapcha.js
@@ -1,5 +1,4 @@
 import { VueReCaptcha } from 'vue-recaptcha-v3';
 import Vue from 'vue';
 
-console.log(process.env.RECAPCHA_SITE_KEY);
 Vue.use(VueReCaptcha, { siteKey: process.env.RECAPCHA_SITE_KEY })
