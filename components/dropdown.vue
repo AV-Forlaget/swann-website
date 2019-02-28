9db7d7dacc3e9e461ae79e7c2ac6604f435be495
Kasper Baagø Jensen <kasper@edentic.dk>
2019-02-28 11:18:40 +0100

    Added search selector in dropdown

--- components/dropdown.vue
+++ components/dropdown.vue
@@ -5,8 +5,9 @@
                 <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 395.65 223" class="icon-dropdown"><path d="M370.5,0a25,25,0,0,0-17.79,7.37L210.57,149.51l-12.73,12.73-12.73-12.73L43,7.37A25.16,25.16,0,1,0,7.38,43L180,215.63a25.16,25.16,0,0,0,35.58,0L388.29,43A25.17,25.17,0,0,0,370.5,0Z"/></svg>
             </div>
             <div class="search-selector--options" v-if="open">
+                <input-field v-if="search" name="Filter..." :value="searchVal" @input="(val) => searchVal = val" ref="searchField"></input-field>
                 <ul>
-                   <li v-for="option in options" 
+                   <li v-for="option in optionList" 
                         :key="(option.value) ? option.value : option"
                         v-text="(option.text) ? option.text : option"
                         :class="{'selected': option == selectedValue}"
@@ -18,17 +19,20 @@
 </template>
 
 <script>
+import InputField from './input-field.vue';
 
 export default {
     props: {
         name: {type: String, default: null},
         value: '',
         options: {type: Array, default() { return [ ]}},
-        placeholder: {type: String, default: ''}
+        placeholder: {type: String, default: ''},
+        search: {type: Boolean, default: false}
     },
     data() {
         return {
             open: false,
+            searchVal: ''
         }
     },
     beforeDestroy() {
@@ -42,6 +46,7 @@ export default {
             this.closeSelector();
         },
         closeSelector() {
+            this.searchVal = '';
             this.open = false;
             window.removeEventListener('click', this.windowListener);
         },
@@ -52,6 +57,7 @@ export default {
             }
 
             this.open = true;
+            this.searchVal = '';
             window.addEventListener('click', this.windowListener);
         },
         windowListener(evt) {
@@ -63,6 +69,19 @@ export default {
         }
     },
     computed: {
+        optionList() {
+            if(!this.searchVal) {
+                return this.options;
+            }
+
+            let regex = new RegExp('^' + this.searchVal + '+', 'i');
+            let options = this.options.filter((item) => {
+                let value = item.text || item;
+                return value.match(regex) || (!this.searchVal);
+            })
+
+            return options;
+        },
         selectedValue() {
             let option = this.options.filter((item) => {
                 return (this.value == (item.value || item));
@@ -81,6 +100,9 @@ export default {
 
             return this.placeholder;
         }
+    },
+    components: {
+        InputField
     }
 }
 </script>
