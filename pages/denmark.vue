5a598c2a8d314fb841812d11ef8c06693209d7ac
Frederik Stahlfest Møller <frederik@edentic.dk>
2019-04-02 12:29:23 +0200

    Added demmark example page

---
+++ pages/denmark.vue
@@ -0,0 +1,51 @@
+<template>
+  <section class="main-content">
+    <div class="main-hero">
+      <div class="main-hero__wrapper">
+        <h1 class="main-hero__heading">Denmark</h1>
+        <p class="main-hero__subheading">[Extended text about our history with the country and current activities]</p>
+      </div>
+   
+    </div>
+    <div class="nice content-section">
+      <h2 class="nice__title">[how nice we are]</h2>
+      <p class="nice__paragraph content-section__paragraph">[more about the niceness]</p>
+    </div>
+    <div class="nice content-section">
+      <div class="ui-block">
+        <div class="ui-block__header">
+          <h3 class="ui-block__header__heading">Narrators</h3>
+        </div>
+        <div class="ui-block__body">
+          <p class="ui-block__body__intro">[Extended text about our history with narrators in the country and current activities + country specific narration stuff]</p>
+          <div class="narrator-grid">
+            <div class="narrator-item">
+              <div class="narrator-item__avtar">
+                <img src="" alt="">
+              </div>
+              <div class="narrator-item__content">
+                <h4 class="narrator-name">Narrator name</h4>
+                <span class="narrator-genre">Genre</span>
+                <span class="narrator-narrations">20</span>
+                <span class="narrator-price">Price</span>
+              </div>
+              <div class="narrator-item__preview">
+                <img src="" alt="">
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+</template>
+
+<script>
+/*
+export default {
+  mounted() {
+    this.$store.commit('step', 1);
+  }
+}
+*/
+</script>
