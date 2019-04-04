3e4f1dd97c41ee7d77de509441f815fd9de04407
Frederik Stahlfest Møller <frederik@edentic.dk>
2019-04-04 16:09:58 +0200

    Added and styled distribution

--- pages/distribution.vue
+++ pages/distribution.vue
@@ -6,6 +6,139 @@
         <p class="main-hero__subheading">[Extended text about our history with Distribution in general and current activities]</p>
       </div>
     </div>
+    <div class="content-section">
+      <div class="content-section__wrapper">
+        <div class="ui-block">
+          <div class="ui-block__header">
+            <h3 class="ui-block__header__heading">Digital audiobooks</h3>
+          </div>
+          <div class="ui-block__body">
+          <p class="ui-block__body__intro">[text about how distribution of digital audiobooks works in general]</p>
+          <div class="icon-list">
+
+            <div class="icon-list__item">
+              <img src="@/assets/img/icons/icon-audiofiles.svg" class="icon-list__icon" alt="" />
+              <h4 class="icon-list__heading">Audiofiles</h4>
+              <p class="icon-list__paragraph">[start of the process, there are files]</p>
+            </div>
+
+            <div class="icon-list__item">
+              <img src="@/assets/img/icons/icon-distributor.svg" class="icon-list__icon" alt="" />
+              <h4 class="icon-list__heading">Distributor</h4>
+              <p class="icon-list__paragraph">[middle of the process, files are sent to server, in general]</p>
+            </div>
+
+            <div class="icon-list__item">
+              <img src="@/assets/img/icons/icon-user.svg" class="icon-list__icon" alt="" />
+              <h4 class="icon-list__heading">User</h4>
+              <p class="icon-list__paragraph">[end of the process, user can listen to product]</p>
+            </div>
+          </div>
+
+          </div>
+        </div>
+
+        <div class="ui-block">
+          <div class="ui-block__header">
+            <h3 class="ui-block__header__heading">Print on Demand</h3>
+          </div>
+          <div class="ui-block__body">
+          <p class="ui-block__body__intro">[text about how distribution of analog audiobooks works in general]</p>
+          <div class="icon-list">
+
+            <div class="icon-list__item">
+              <img src="@/assets/img/icons/icon-audiobook-cover.svg" class="icon-list__icon" alt="" />
+              <h4 class="icon-list__heading">Audiobook Cover</h4>
+              <p class="icon-list__paragraph">[text about how cover production works in general, and how good we are at it]</p>
+            </div>
+
+            <div class="icon-list__item">
+              <img src="@/assets/img/icons/icon-registration.svg" class="icon-list__icon" alt="" />
+              <h4 class="icon-list__heading">Registration</h4>
+              <p class="icon-list__paragraph">[text about how registation works, country specific]</p>
+            </div>
+
+            <div class="icon-list__item">
+              <img src="@/assets/img/icons/icon-production.svg" class="icon-list__icon" alt="" />
+              <h4 class="icon-list__heading">Production</h4>
+              <p class="icon-list__paragraph">[production workflow, our roles and the customers roles, in general]</p>
+            </div>
+
+             <div class="icon-list__item">
+              <img src="@/assets/img/icons/icon-shipment.svg" class="icon-list__icon" alt="" />
+              <h4 class="icon-list__heading">Shipment</h4>
+              <p class="icon-list__paragraph">[how shipment and inventory works, in general]</p>
+            </div>
+          </div>
+
+          </div>
+        </div>
+
+        <div class="ui-block">
+          <div class="ui-block__header">
+            <h3 class="ui-block__header__heading">Publishing</h3>
+          </div>
+          <div class="ui-block__body">
+          <p class="ui-block__body__intro">Lorem ipsum dolor sit amet consectetur adipisicing elit. Consectetur, assumenda laborum adipisci sed, et a nisi totam optio doloremque quae mollitia deleniti! Ea suscipit, ipsa ullam doloremque mollitia.</p>
+          <div class="icon-list">
+
+            <div class="icon-list__item">
+              <img src="@/assets/img/icons/icon-cover-production.svg" class="icon-list__icon" alt="" />
+              <h4 class="icon-list__heading">Cover production</h4>
+              <p class="icon-list__paragraph">[how awesome our graphic designers are, and that they can produce covers]</p>
+            </div>
+
+            <div class="icon-list__item">
+              <img src="@/assets/img/icons/icon-royalty.svg" class="icon-list__icon" alt="" />
+              <h4 class="icon-list__heading">Royalty</h4>
+              <p class="icon-list__paragraph">[how royalty works]</p>
+            </div>
+
+            <div class="icon-list__item">
+              <img src="@/assets/img/icons/icon-consultance.svg" class="icon-list__icon" alt="" />
+              <h4 class="icon-list__heading">Consultance</h4>
+              <p class="icon-list__paragraph">[how much we know, and that we share that information, in general]</p>
+            </div>
+
+             <div class="icon-list__item">
+              <img src="@/assets/img/icons/icon-translation.svg" class="icon-list__icon" alt="" />
+              <h4 class="icon-list__heading">Translation</h4>
+              <p class="icon-list__paragraph">[we can translate, we operate in many countries]</p>
+            </div>
+          </div>
+
+          </div>
+        </div>
+
+         <div class="ui-block">
+          <div class="ui-block__header">
+            <h3 class="ui-block__header__heading">Streaming</h3>
+          </div>
+          <div class="ui-block__body">
+          <p class="ui-block__body__intro">[text about how streaming works in in general]</p>
+          <div class="icon-list">
+
+            <div class="icon-list__item">
+              <img src="@/assets/img/logos/logo-mofibo.svg" class="icon-list__icon icon-list__item--small" alt="" />
+            </div>
+
+            <div class="icon-list__item">
+              <img src="@/assets/img/logos/logo-storytel.svg" class="icon-list__icon icon-list__item--small" alt="" />
+            </div>
+
+            <div class="icon-list__item">
+              <img src="@/assets/img/logos/logo-nextory.svg" class="icon-list__icon" alt="" />
+            </div>
+
+             <div class="icon-list__item icon-list__item--large">
+              <img src="@/assets/img/logos/logo-saxo.svg" class="icon-list__icon icon-list__item--large" alt="" />
+            </div>
+          </div>
+          <p class="ui-block__body__intro">[streaming services from all countries]</p>
+          </div>
+        </div>
+      </div>
+    </div>
   </section>
 </template>
 
