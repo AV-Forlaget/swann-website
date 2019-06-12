f7d9f56bfcc3c944675321c3fd6d4438e1f09b6c
Frederik Stahlfest Møller <frederik@edentic.dk>
2019-06-12 15:02:08 +0200

    Styled about us page

--- pages/about-us.vue
+++ pages/about-us.vue
@@ -13,156 +13,127 @@
 
 	<div class="content-section content-section--white">
 		<div class="content-section__wrapper">
-
-			<div class="department">
-				<h2 class="department__heading">Management</h2>
-				<div class="department__list">
-				  	<div class="department__item employee">
-						<img src="@/assets/img/employees/default.jpg" class="employee__image" alt="" />
-						<h3 class="employee__name" >Jeremiah Archer</h3>
-						<p class="employee__jobtitle">CEO</p>
-						<p class="employee__email"><nuxt-link to="mailto:ja@swann.com" class="employee__anchor">ja@swann.com</nuxt-link></p>
-					</div>
-				  	<div class="department__item employee">
-						<img src="@/assets/img/employees/default.jpg" class="employee__image" alt="" />
-						<h3 class="employee__name" >Anthony Patel</h3>
-						<p class="employee__jobtitle">Chairman</p>
-						<p class="employee__email"><nuxt-link to="mailto:ap@swann.com" class="employee__anchor">ap@swann.com</nuxt-link></p>
-					</div>
-				  	<div class="department__item employee">
-						<img src="@/assets/img/employees/default.jpg" class="employee__image" alt="" />
-						<h3 class="employee__name" >Mya Chase</h3>
-						<p class="employee__jobtitle">Head of production</p>
-						<p class="employee__email"><nuxt-link to="mailto:mc@swann.com" class="employee__anchor">mc@swann.com</nuxt-link></p>
-					</div>
+			<h2 class="content-section__heading content-section__heading--center">Management</h2>
+			<div class="department-grid">
+				<div class="department-grid__item">
+					<h3 class="employee-name" >Julian Lundsgaard</h3>
+					<p class="employee-jobtitle">CEO</p>
+					<p class="employee-email"><nuxt-link to="mailto:ja@swann.com" class="employee__anchor">ja@swann.com</nuxt-link></p>
+				</div>
+				<div class="department-grid__item">
+					<h3 class="employee-name" >Daniel Lundsgaard</h3>
+					<p class="employee-jobtitle">Project manager</p>
+					<p class="employee-email"><nuxt-link to="mailto:ap@swann.com" class="employee__anchor">ap@swann.com</nuxt-link></p>
+				</div>
+				<div class="department-grid__item">
+					<h3 class="employee-name" >Lene Christensen</h3>
+					<p class="employee-jobtitle">Head of production</p>
+					<p class="employee-email"><nuxt-link to="mailto:mc@swann.com" class="employee__anchor">mc@swann.com</nuxt-link></p>
 				</div>
 			</div>
-
-
-			<div class="department">
-				<h2 class="department__heading">Publishing</h2>
-				<div class="department__list">
-				  	<div class="department__item employee">
-						<img src="@/assets/img/employees/default.jpg" class="employee__image" alt="" />
-						<h3 class="employee__name" >Solomon Hancock</h3>
-						<p class="employee__jobtitle">Editor-in-chief</p>
-						<p class="employee__email"><nuxt-link to="mailto:sh@swann.com" class="employee__anchor">sh@swann.com</nuxt-link></p>
-					</div>
-				</div>
-			</div>
-
-
-			<div class="department">
-				<h2 class="department__heading">Audio production</h2>
-				<div class="department__list">
-				  	<div class="department__item employee">
-						<img src="@/assets/img/employees/default.jpg" class="employee__image" alt="" />
-						<h3 class="employee__name" >Angus Guzman</h3>
-						<p class="employee__jobtitle">Chief audio tech</p>
-						<p class="employee__email"><nuxt-link to="mailto:ag@swann.com" class="employee__anchor">ag@swann.com</nuxt-link></p>
-					</div>
-				  	<div class="department__item employee">
-						<img src="@/assets/img/employees/default.jpg" class="employee__image" alt="" />
-						<h3 class="employee__name" >Garfield Garrison</h3>
-						<p class="employee__jobtitle">Audio research</p>
-						<p class="employee__email"><nuxt-link to="mailto:gg@swann.com" class="employee__anchor">gg@swann.com</nuxt-link></p>
-					</div>
-				  	<div class="department__item employee">
-						<img src="@/assets/img/employees/default.jpg" class="employee__image" alt="" />
-						<h3 class="employee__name" >Billy Elliott</h3>
-						<p class="employee__jobtitle">Audio tech</p>
-						<p class="employee__email"><nuxt-link to="mailto:be@swann.com" class="employee__anchor">be@swann.com</nuxt-link></p>
-					</div>
-				  	<div class="department__item employee">
-						<img src="@/assets/img/employees/default.jpg" class="employee__image" alt="" />
-						<h3 class="employee__name" >Aysha Craig</h3>
-						<p class="employee__jobtitle">Digitization</p>
-						<p class="employee__email"><nuxt-link to="mailto:ac@swann.com" class="employee__anchor">ac@swann.com</nuxt-link></p>
-					</div>
-				</div>
-			</div>
-
-
-			<div class="department">
-				<h2 class="department__heading">Distribution/Production</h2>
-				<div class="department__list">
-				  	<div class="department__item employee">
-						<img src="@/assets/img/employees/default.jpg" class="employee__image" alt="" />
-						<h3 class="employee__name" >Ellie-Mae Brady</h3>
-						<p class="employee__jobtitle">Graphic designer</p>
-						<p class="employee__email"><nuxt-link to="mailto:eb@swann.com" class="employee__anchor">eb@swann.com</nuxt-link></p>
-					</div>
-				  	<div class="department__item employee">
-						<img src="@/assets/img/employees/default.jpg" class="employee__image" alt="" />
-						<h3 class="employee__name" >Lauren Cantrell</h3>
-						<p class="employee__jobtitle">Coordinator/controller</p>
-						<p class="employee__email"><nuxt-link to="mailto:lc@swann.com" class="employee__anchor">lc@swann.com</nuxt-link></p>
-					</div>
-				  	<div class="department__item employee">
-						<img src="@/assets/img/employees/default.jpg" class="employee__image" alt="" />
-						<h3 class="employee__name" >Naomi Bass</h3>
-						<p class="employee__jobtitle">Coordinator/shipment</p>
-						<p class="employee__email"><nuxt-link to="mailto:nb@swann.com" class="employee__anchor">nb@swann.com</nuxt-link></p>
-					</div>
-				</div>
-			</div>
-
-
-			<div class="department">
-				<h2 class="department__heading">Development</h2>
-				<div class="department__list">
-				  	<div class="department__item employee">
-						<img src="@/assets/img/employees/default.jpg" class="employee__image" alt="" />
-						<h3 class="employee__name" >Wayne Lester</h3>
-						<p class="employee__jobtitle">Lead programmer</p>
-						<p class="employee__email"><nuxt-link to="mailto:wl@swann.com" class="employee__anchor">wl@swann.com</nuxt-link></p>
-					</div>
-				  	<div class="department__item employee">
-						<img src="@/assets/img/employees/default.jpg" class="employee__image" alt="" />
-						<h3 class="employee__name" >Kieron Christensen</h3>
-						<p class="employee__jobtitle">Backend developer</p>
-						<p class="employee__email"><nuxt-link to="mailto:lc@swann.com" class="employee__anchor">lc@swann.com</nuxt-link></p>
-					</div>
-				</div>
-			</div>
-
-
-			<div class="department">
-				<h2 class="department__heading">Bookkeeping</h2>
-				<div class="department__list">
-				  	<div class="department__item employee">
-						<img src="@/assets/img/employees/default.jpg" class="employee__image" alt="" />
-						<h3 class="employee__name" >Gabriella Lozano</h3>
-						<p class="employee__jobtitle">Bookkeeper</p>
-						<p class="employee__email"><nuxt-link to="mailto:gl@swann.com" class="employee__anchor">gl@swann.com</nuxt-link></p>
-					</div>
-				  	<div class="department__item employee">
-						<img src="@/assets/img/employees/default.jpg" class="employee__image" alt="" />
-						<h3 class="employee__name" >Cassie Mcdonald</h3>
-						<p class="employee__jobtitle">Bookkeeper</p>
-						<p class="employee__email"><nuxt-link to="mailto:cm@swann.com" class="employee__anchor">cm@swann.com</nuxt-link></p>
-					</div>
-				</div>
-			</div>
-
-<!--
-
-
-<div class="department">
-	<h2 class="department__heading"></h2>
-	<div class="department__list">
-	  	<div class="department__item employee">
-			<img src="@/assets/img/employees/default.jpg" class="employee__image" alt="" />
-			<h3 class="employee__name" ></h3>
-			<p class="employee__jobtitle"></p>
-			<p class="employee__email"><nuxt-link to="mailto:" class="employee__anchor"></nuxt-link></p>
 		</div>
 	</div>
-</div>
--->
+	<div class="content-section content-section--white">
+		<div class="content-section__wrapper">
+			<h2 class="content-section__heading content-section__heading--center">Audio production</h2>
+			<div class="department-grid">
+				<div class="department-grid__item">
+					<h3 class="employee-name" >Benjamin Blaaberg</h3>
+					<p class="employee-jobtitle">Chief audio tech</p>
+					<p class="employee-email"><nuxt-link to="mailto:ag@swann.com" class="employee__anchor">ag@swann.com</nuxt-link></p>
+				</div>
+				<div class="department-grid__item">
+					<h3 class="employee-name" >Oliver Lundsgaard</h3>
+					<p class="employee-jobtitle">Audio research</p>
+					<p class="employee-email"><nuxt-link to="mailto:gg@swann.com" class="employee__anchor">gg@swann.com</nuxt-link></p>
+				</div>
+				<div class="department-grid__item">
+					<h3 class="employee-name" >Henrik Pedersen</h3>
+					<p class="employee-jobtitle">Audio tech</p>
+					<p class="employee-email"><nuxt-link to="mailto:be@swann.com" class="employee__anchor">be@swann.com</nuxt-link></p>
+				</div>
+				<div class="department-grid__item">
+					<h3 class="employee-name" >Bahra Aki Chamanara</h3>
+					<p class="employee-jobtitle">Digitization</p>
+					<p class="employee-email"><nuxt-link to="mailto:ac@swann.com" class="employee__anchor">ac@swann.com</nuxt-link></p>
+				</div>
+			</div>
+		</div>
+	</div>
+	<div class="content-section content-section--white">
+		<div class="content-section__wrapper">
+			<h2 class="content-section__heading content-section__heading--center">Publishing</h2>
+			<div class="department-grid">
+				<div class="department-grid__item">
+					<h3 class="employee-name" >Martin Johs. Møller</h3>
+					<p class="employee-jobtitle">Editor-in-chief</p>
+					<p class="employee-email"><nuxt-link to="mailto:sh@swann.com" class="employee__anchor">sh@swann.com</nuxt-link></p>
+				</div>
+			</div>
+		</div>
+	</div>
+	<div class="content-section content-section--white">
+		<div class="content-section__wrapper">
+			<h2 class="content-section__heading content-section__heading--center">Distribution</h2>
+			<div class="department-grid">
+				<div class="department-grid__item">
+					<h3 class="employee-name" >Malin Flinta</h3>
+					<p class="employee-jobtitle">Coordinator/controller</p>
+					<p class="employee-email"><nuxt-link to="mailto:lc@swann.com" class="employee__anchor">lc@swann.com</nuxt-link></p>
+				</div>
+				<div class="department-grid__item">
+					<h3 class="employee-name" >Natja Sørensen</h3>
+					<p class="employee-jobtitle">Graphic designer</p>
+					<p class="employee-email"><nuxt-link to="mailto:eb@swann.com" class="employee__anchor">eb@swann.com</nuxt-link></p>
+				</div>
+				<div class="department-grid__item">
+					<h3 class="employee-name" >?</h3>
+					<p class="employee-jobtitle">Coordinator/shipment</p>
+					<p class="employee-email"><nuxt-link to="mailto:nb@swann.com" class="employee__anchor">nb@swann.com</nuxt-link></p>
+				</div>
+			</div>
+		</div>
+	</div>
+	<div class="content-section content-section--white">
+		<div class="content-section__wrapper">
+			<h2 class="content-section__heading content-section__heading--center">Development</h2>
+			<div class="department-grid">
+				<div class="department-grid__item">
+					<h3 class="employee-name" >Jakob Lund</h3>
+					<p class="employee-jobtitle">Lead programmer</p>
+					<p class="employee--email"><nuxt-link to="mailto:wl@swann.com" class="employee__anchor">wl@swann.com</nuxt-link></p>
+				</div>
+				<div class="department-grid__item">
+					<h3 class="employee-name" >Tore Sæderup</h3>
+					<p class="employee-jobtitle">Backend developer</p>
+					<p class="employee-email"><nuxt-link to="mailto:lc@swann.com" class="employee__anchor">lc@swann.com</nuxt-link></p>
+				</div>
+				<div class="department-grid__item">
+					<h3 class="employee-name" >Klaus Fyhn Jacobsen</h3>
+					<p class="employee-jobtitle">Backend developer</p>
+					<p class="employee-email"><nuxt-link to="mailto:lc@swann.com" class="employee__anchor">lc@swann.com</nuxt-link></p>
+				</div>
+			</div>
+		</div>
+	</div>
 
-		</div> <!-- END .content-section__wrapper -->
-	</div> <!-- END .content-section__wrapper -->
+	<div class="content-section content-section--white">
+		<div class="content-section__wrapper">
+			<h2 class="content-section__heading content-section__heading--center">Bookkeeping</h2>
+			<div class="department-grid">
+				<div class="department-grid__item">
+					<h3 class="employee-name" >Jane Olsen</h3>
+					<p class="employee-jobtitle">Bookkeeper</p>
+					<p class="employee-email"><nuxt-link to="mailto:gl@swann.com" class="employee__anchor">gl@swann.com</nuxt-link></p>
+				</div>
+				<div class="department-grid__item">
+					<h3 class="employee-name" >Anette Lundsgaard</h3>
+					<p class="employee-jobtitle">Bookkeeper</p>
+					<p class="employee-email"><nuxt-link to="mailto:cm@swann.com" class="employee__anchor">cm@swann.com</nuxt-link></p>
+				</div>
+			</div>
+		</div>
+	</div>
   </section>
 </template>
 
