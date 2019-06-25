0cbe15acceadb3e7378592202d591dc87d843426
Kasper Baagø Jensen <kasper@edentic.dk>
2019-06-25 13:55:53 +0200

    Fixed issues with production build and robots.txt file

--- build.sh
+++ build.sh
@@ -3,6 +3,7 @@ docker-compose build
 docker-compose up -d
 
 echo "*** Generating build ***"
+rm -rf dist
 docker-compose exec frontend yarn generate
 
 echo "Visit the static build at http://localhost"
