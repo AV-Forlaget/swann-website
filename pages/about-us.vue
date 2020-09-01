c090350d397fff2016bd034fa3b7cd221043f218
oskarasg <o.goborovas@yahoo.com>
2020-09-01 13:01:39 +0200

    removed employees

--- pages/about-us.vue
+++ pages/about-us.vue
@@ -1,166 +1,182 @@
 <template>
   <section class="main-content">
-
-	<div class="content-section frontpage-audiobooks">
+    <div class="content-section frontpage-audiobooks">
       <div class="content-section__wrapper">
-			<h1 class="content-section__heading content-section__heading--center">Swann Studio</h1>
-            <p class="content-section__text">Swann Studio is a family owned international audiobook production company and publishing house established in Odense, Denmark in 1983. The business has grown through several stages and market transitions to become an international frontrunner in audiobook production both in regards to volume and quality.</p>
-            <p class="content-section__text">We have a deep and well rooted understanding of all aspects of the industry, having helped develop and nurture the strongly established Danish market over a span of four decades.</p>
-            <p class="content-section__text">Swann Studio is very focused on incorporating solid and accessible tech solutions to make it as easy as possible to order and receive a production, and for our clients and us to have the most seamless communication possible. We offer all audiobook production services imaginable, provide audiobook business consultancy and assist in everything from designing a cover to helping with strategies for regional markets.</p>
+        <h1 class="content-section__heading content-section__heading--center">Swann Studio</h1>
+        <p
+          class="content-section__text"
+        >Swann Studio is a family owned international audiobook production company and publishing house established in Odense, Denmark in 1983. The business has grown through several stages and market transitions to become an international frontrunner in audiobook production both in regards to volume and quality.</p>
+        <p
+          class="content-section__text"
+        >We have a deep and well rooted understanding of all aspects of the industry, having helped develop and nurture the strongly established Danish market over a span of four decades.</p>
+        <p
+          class="content-section__text"
+        >Swann Studio is very focused on incorporating solid and accessible tech solutions to make it as easy as possible to order and receive a production, and for our clients and us to have the most seamless communication possible. We offer all audiobook production services imaginable, provide audiobook business consultancy and assist in everything from designing a cover to helping with strategies for regional markets.</p>
       </div>
     </div>
 
+    <div class="content-section content-section--bg-color">
+      <div class="content-section__wrapper">
+        <h2 class="content-section__heading content-section__heading--center">Management</h2>
+        <div class="department-grid">
+          <div class="department-grid__item">
+            <img src="@/assets/img/employees/julian.jpg" alt class="employee-img" />
+            <h3 class="employee-name">Julian Lundsgaard</h3>
+            <p class="employee-jobtitle">CEO</p>
+            <p class="employee-email">
+              <a href="mailto:jwl@swann-studio.com">jwl@swann-studio.com</a>
+            </p>
+          </div>
+          <div class="department-grid__item">
+            <img src="@/assets/img/employees/daniel.jpg" alt class="employee-img" />
+            <h3 class="employee-name">Daniel Lundsgaard</h3>
+            <p class="employee-jobtitle">Project Manager</p>
+            <p class="employee-email">
+              <a href="mailto:dwl@swann-studio.com">dwl@swann-studio.com</a>
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="content-section">
+      <div class="content-section__wrapper">
+        <h2 class="content-section__heading content-section__heading--center">Audio production</h2>
+        <div class="department-grid">
+          <div class="department-grid__item">
+            <img src="@/assets/img/employees/lene.jpg" alt class="employee-img" />
+            <h3 class="employee-name">Lene Christensen</h3>
+            <p class="employee-jobtitle">Head of Production</p>
+            <p class="employee-email">
+              <a href="mailto:lc@swann-studio.com">lc@swann-studio.com</a>
+            </p>
+          </div>
+          <div class="department-grid__item">
+            <img src="@/assets/img/employees/benjamin.jpg" alt class="employee-img" />
+            <h3 class="employee-name">Benjamin Blaabjerg</h3>
+            <p class="employee-jobtitle">Chief Audio Engineer</p>
+            <p class="employee-email">
+              <a href="mailto:bb@swann-studio.com">bb@swann-studio.com</a>
+            </p>
+          </div>
+          <div class="department-grid__item">
+            <img src="@/assets/img/employees/oliver.jpg" alt class="employee-img" />
+            <h3 class="employee-name">Oliver Lundsgaard</h3>
+            <p class="employee-jobtitle">Audio Research</p>
+            <p class="employee-email">
+              <a href="mailto:owl@swann-studio.com">owl@swann-studio.com</a>
+            </p>
+          </div>
+          <div class="department-grid__item">
+            <img src="@/assets/img/employees/henrik.jpg" alt class="employee-img" />
+            <h3 class="employee-name">Henrik Pedersen</h3>
+            <p class="employee-jobtitle">Audio Tech</p>
+            <p class="employee-email">
+              <a href="mailto:hp@swann-studio.com">hp@swann-studio.com</a>
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="content-section content-section--bg-color">
+      <div class="content-section__wrapper">
+        <h2 class="content-section__heading content-section__heading--center">Publishing</h2>
+        <div class="department-grid">
+          <div class="department-grid__item">
+            <img src="@/assets/img/employees/martin.jpg" alt class="employee-img" />
+            <h3 class="employee-name">Martin Johs. Møller</h3>
+            <p class="employee-jobtitle">Editor-in-Chief</p>
+            <p class="employee-email">
+              <a href="mailto:mjm@swann-studio.com">mjm@swann-studio.com</a>
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="content-section">
+      <div class="content-section__wrapper">
+        <h2 class="content-section__heading content-section__heading--center">Distribution</h2>
+        <div class="department-grid">
+          <div class="department-grid__item">
+            <img src="@/assets/img/employees/malin.jpg" alt class="employee-img" />
+            <h3 class="employee-name">Malin Flinta</h3>
+            <p class="employee-jobtitle">Coordinator</p>
+            <p class="employee-email">
+              <a href="mailto:mf@swann-studio.com">mf@swann-studio.com</a>
+            </p>
+          </div>
+          <div class="department-grid__item">
+            <img src="@/assets/img/employees/helle.jpg" alt class="employee-img" />
+            <h3 class="employee-name">Helle Cramer Krogsøe</h3>
+            <p class="employee-jobtitle">Coordinator</p>
+            <p class="employee-email">
+              <a href="mailto:hck@swann-studio.com">hck@swann-studio.com</a>
+            </p>
+          </div>
+          <div class="department-grid__item">
+            <img src="@/assets/img/employees/natja.jpg" alt class="employee-img" />
+            <h3 class="employee-name">Natja Koldby Sørensen</h3>
+            <p class="employee-jobtitle">Graphic Designer</p>
+            <p class="employee-email">
+              <a href="mailto:ns@swann-studio.com">ns@swann-studio.com</a>
+            </p>
+          </div>
+          <div class="department-grid__item">
+            <img src="@/assets/img/employees/lisbeth.jpg" alt class="employee-img" />
+            <h3 class="employee-name">Lisbeth Hornbæk</h3>
+            <p class="employee-jobtitle">Coordinator/Shipment</p>
+            <p class="employee-email">
+              <a href="mailto:lh@swann-studio.com">lh@swann-studio.com</a>
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="content-section content-section--bg-color">
+      <div class="content-section__wrapper">
+        <h2 class="content-section__heading content-section__heading--center">Development</h2>
+        <div class="department-grid">
+          <div class="department-grid__item">
+            <img src="@/assets/img/employees/jakob.jpg" alt class="employee-img" />
+            <h3 class="employee-name">Jakob Lund</h3>
+            <p class="employee-jobtitle">Lead Programmer</p>
+            <p class="employee-email">
+              <a href="mailto:jl@swann-studio.com">jl@swann-studio.com</a>
+            </p>
+          </div>
+          <div class="department-grid__item">
+            <img src="@/assets/img/employees/klaus.jpg" alt class="employee-img" />
+            <h3 class="employee-name">Klaus Fyhn Jacobsen</h3>
+            <p class="employee-jobtitle">Backend Developer</p>
+            <p class="employee-email">
+              <a href="mailto:kfj@swann-studio.com">kfj@swann-studio.com</a>
+            </p>
+          </div>
+          <div class="department-grid__item">
+            <img src="@/assets/img/employees/oskar.jpg" alt class="employee-img" />
+            <h3 class="employee-name">Oskaras Goborovas</h3>
+            <p class="employee-jobtitle">Frontend Developer</p>
+            <p class="employee-email">
+              <a href="mailto:og@swann-studio.com">og@swann-studio.com</a>
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
 
-	<div class="content-section content-section--bg-color">
-		<div class="content-section__wrapper">
-			<h2 class="content-section__heading content-section__heading--center">Management</h2>
-			<div class="department-grid">
-				<div class="department-grid__item">
-					<img src="@/assets/img/employees/julian.jpg" alt="" class="employee-img">
-					<h3 class="employee-name" >Julian Lundsgaard</h3>
-					<p class="employee-jobtitle">CEO</p>
-					<p class="employee-email"><a href="mailto:jwl@swann-studio.com" >jwl@swann-studio.com</a></p>
-				</div>
-				<div class="department-grid__item">
-					<img src="@/assets/img/employees/daniel.jpg" alt="" class="employee-img">
-					<h3 class="employee-name" >Daniel Lundsgaard</h3>
-					<p class="employee-jobtitle">Project Manager</p>
-					<p class="employee-email"><a href="mailto:dwl@swann-studio.com" >dwl@swann-studio.com</a></p>
-				</div>
-			</div>
-		</div>
-	</div>
-	<div class="content-section">
-		<div class="content-section__wrapper">
-			<h2 class="content-section__heading content-section__heading--center">Audio production</h2>
-			<div class="department-grid">
-				<div class="department-grid__item">
-					<img src="@/assets/img/employees/lene.jpg" alt="" class="employee-img">
-					<h3 class="employee-name" >Lene Christensen</h3>
-					<p class="employee-jobtitle">Head of Production</p>
-					<p class="employee-email"><a href="mailto:lc@swann-studio.com">lc@swann-studio.com</a></p>
-				</div>
-				<div class="department-grid__item">
-					<img src="@/assets/img/employees/benjamin.jpg" alt="" class="employee-img">
-					<h3 class="employee-name" >Benjamin Blaaberg</h3>
-					<p class="employee-jobtitle">Chief Audio Tech</p>
-					<p class="employee-email"><a href="mailto:bb@swann-studio.com">bb@swann-studio.com</a></p>
-				</div>
-				<div class="department-grid__item">
-					<img src="@/assets/img/employees/oliver.jpg" alt="" class="employee-img">
-					<h3 class="employee-name" >Oliver Lundsgaard</h3>
-					<p class="employee-jobtitle">Audio Research</p>
-					<p class="employee-email"><a href="mailto:owl@swann-studio.com">owl@swann-studio.com</a></p>
-				</div>
-				<div class="department-grid__item">
-					<img src="@/assets/img/employees/henrik.jpg" alt="" class="employee-img">
-					<h3 class="employee-name" >Henrik Pedersen</h3>
-					<p class="employee-jobtitle">Audio Tech</p>
-					<p class="employee-email"><a href="mailto:hp@swann-studio.com">hp@swann-studio.com</a></p>
-				</div>
-				<div class="department-grid__item">
-					<img src="@/assets/img/employees/bahra.jpg" alt="" class="employee-img">
-					<h3 class="employee-name" >Bahra Aki Chamanara</h3>
-					<p class="employee-jobtitle">Digitisation</p>
-					<p class="employee-email"><a href="mailto:bac@swann-studio.com">bac@swann-studio.com</a></p>
-				</div>
-			</div>
-		</div>
-	</div>
-	<div class="content-section content-section--bg-color">
-		<div class="content-section__wrapper">
-			<h2 class="content-section__heading content-section__heading--center">Publishing</h2>
-			<div class="department-grid">
-				<div class="department-grid__item">
-					<img src="@/assets/img/employees/martin.jpg" alt="" class="employee-img">
-					<h3 class="employee-name" >Martin Johs. Møller</h3>
-					<p class="employee-jobtitle">Editor-in-Chief</p>
-					<p class="employee-email"><a href="mailto:mjm@swann-studio.com">mjm@swann-studio.com</a></p>
-				</div>
-			</div>
-		</div>
-	</div>
-	<div class="content-section">
-		<div class="content-section__wrapper">
-			<h2 class="content-section__heading content-section__heading--center">Distribution</h2>
-			<div class="department-grid">
-				<div class="department-grid__item">
-					<img src="@/assets/img/employees/malin.jpg" alt="" class="employee-img">
-					<h3 class="employee-name" >Malin Flinta</h3>
-					<p class="employee-jobtitle">Coordinator</p>
-					<p class="employee-email"><a href="mailto:mf@swann-studio.com">mf@swann-studio.com</a></p>
-				</div>
-				<div class="department-grid__item">
-					<img src="@/assets/img/employees/helle.jpg" alt="" class="employee-img">
-					<h3 class="employee-name" >Helle Cramer Krogsøe</h3>
-					<p class="employee-jobtitle">Coordinator</p>
-					<p class="employee-email"><a href="mailto:hck@swann-studio.com">hck@swann-studio.com</a></p>
-				</div>
-				<div class="department-grid__item">
-					<img src="@/assets/img/employees/natja.jpg" alt="" class="employee-img">
-					<h3 class="employee-name" >Natja Koldby Sørensen</h3>
-					<p class="employee-jobtitle">Graphic Designer</p>
-					<p class="employee-email"><a href="mailto:ns@swann-studio.com">ns@swann-studio.com</a></p>
-				</div>
-				<div class="department-grid__item">
-					<img src="@/assets/img/employees/lisbeth.jpg" alt="" class="employee-img">
-					<h3 class="employee-name" >Lisbeth Hornbæk</h3>
-					<p class="employee-jobtitle">Coordinator/Shipment</p>
-					<p class="employee-email"><a href="mailto:lh@swann-studio.com">lh@swann-studio.com</a></p>
-				</div>
-			</div>
-		</div>
-	</div>
-	<div class="content-section content-section--bg-color">
-		<div class="content-section__wrapper">
-			<h2 class="content-section__heading content-section__heading--center">Development</h2>
-			<div class="department-grid">
-				<div class="department-grid__item">
-					<img src="@/assets/img/employees/jakob.jpg" alt="" class="employee-img">
-					<h3 class="employee-name" >Jakob Lund</h3>
-					<p class="employee-jobtitle">Lead Programmer</p>
-					<p class="employee-email"><a href="mailto:jl@swann-studio.com">jl@swann-studio.com</a></p>
-				</div>
-				<div class="department-grid__item">
-					<img src="@/assets/img/employees/tore.jpg" alt="" class="employee-img">
-					<h3 class="employee-name" >Tore Sæderup</h3>
-					<p class="employee-jobtitle">Backend Developer</p>
-					<p class="employee-email"><a href="mailto:ts@swann-studio.com">ts@swann-studio.com</a></p>
-				</div>
-				<div class="department-grid__item">
-					<img src="@/assets/img/employees/klaus.jpg" alt="" class="employee-img">
-					<h3 class="employee-name" >Klaus Fyhn Jacobsen</h3>
-					<p class="employee-jobtitle">Backend Developer</p>
-					<p class="employee-email"><a href="mailto:kfj@swann-studio.com">kfj@swann-studio.com</a></p>
-				</div>
-				<div class="department-grid__item">
-					<img src="@/assets/img/employees/oskar.jpg" alt="" class="employee-img">
-					<h3 class="employee-name" >Oskaras Goborovas</h3>
-					<p class="employee-jobtitle">Frontend developer</p>
-					<p class="employee-email"><a href="mailto:og@swann-studio.com">og@swann-studio.com</a></p>
-				</div>
-			</div>
-		</div>
-	</div>
-
-	<div class="content-section">
-		<div class="content-section__wrapper">
-			<h2 class="content-section__heading content-section__heading--center">Bookkeeping</h2>
-			<div class="department-grid">
-				<div class="department-grid__item">
-					<img src="@/assets/img/employees/jane.jpg" alt="" class="employee-img">
-					<h3 class="employee-name" >Jane Olsen</h3>
-					<p class="employee-jobtitle">Bookkeeper</p>
-					<p class="employee-email"><a href="mailto:invoice@swann-studio.com" >invoice@swann-studio.com</a></p>
-				</div>
-				<div class="department-grid__item">
-					<img src="@/assets/img/employees/annette.jpg" alt="" class="employee-img">
-					<h3 class="employee-name" >Anette Lundsgaard</h3>
-					<p class="employee-jobtitle">Bookkeeper</p>
-					<p class="employee-email"><a href="mailto:awl@swann-studio.com" >awl@swann-studio.com</a></p>
-				</div>
-			</div>
-		</div>
-	</div>
+    <div class="content-section">
+      <div class="content-section__wrapper">
+        <h2 class="content-section__heading content-section__heading--center">Bookkeeping</h2>
+        <div class="department-grid">
+          <div class="department-grid__item">
+            <img src="@/assets/img/employees/annette.jpg" alt class="employee-img" />
+            <h3 class="employee-name">Anette Lundsgaard</h3>
+            <p class="employee-jobtitle">Bookkeeper</p>
+            <p class="employee-email">
+              <a href="mailto:awl@swann-studio.com">awl@swann-studio.com</a>
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
   </section>
 </template>
