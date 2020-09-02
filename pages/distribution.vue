b85c5925d8c39dbdb2bc532e8d3c2461b146adcf
oskarasg <o.goborovas@yahoo.com>
2020-09-02 10:30:21 +0200

    logos added/deleted small css change

--- pages/distribution.vue
+++ pages/distribution.vue
@@ -30,25 +30,29 @@
     <div class="content-section content-section--white">
       <div class="content-section__wrapper">
         <h2 class="content-section__heading content-section__heading--center">Digital distribution</h2>
-        <p class="content-section__text">Digital distribution can be handled directly through our web application and we can easily set up our API to deliver a publication to any additional destinations. We work with all relevant streaming services and/or other sales channels. By popular demand, we also offer track division of audiobooks to make them compatible for direct upload to music streaming services.</p>
-        <p class="content-section__text">If you are interested in our digital distribution services, please contact our distribution team.</p>
+        <p
+          class="content-section__text"
+        >Digital distribution can be handled directly through our web application and we can easily set up our API to deliver a publication to any additional destinations. We work with all relevant streaming services and/or other sales channels. By popular demand, we also offer track division of audiobooks to make them compatible for direct upload to music streaming services.</p>
+        <p
+          class="content-section__text"
+        >If you are interested in our digital distribution services, please contact our distribution team.</p>
         <div class="content-section__btn content-section__btn--center">
           <nuxt-link class="btn" to="/about-us">Contact information</nuxt-link>
         </div>
         <div class="distribution-block">
           <div class="distribution-block__grid">
             <div class="distribution-block__grid__item distribution-block__grid__item--left">
-              <img src="@/assets/img/icons/icon-swann-api.png" class="swann-api-img" alt="" />
-                <h2>Swann Studio API</h2>
-                <p>The required material is automatically send to the relevant sales channels and streaming services with the help of our software. If you are an existing user of our audio production services our convenient distribution can be added on with a few clicks.</p>
+              <img src="@/assets/img/icons/icon-swann-api.png" class="swann-api-img" alt />
+              <h2>Swann Studio API</h2>
+              <p>The required material is automatically send to the relevant sales channels and streaming services with the help of our software. If you are an existing user of our audio production services our convenient distribution can be added on with a few clicks.</p>
             </div>
             <div class="distribution-block__grid__item distribution-block__grid__item--right">
               <div class="distribution-block__grid__sub-item">
-                <img src="@/assets/img/icons/icon-server.png" class="icon-server-img" alt="" />
+                <img src="@/assets/img/icons/icon-server.png" class="icon-server-img" alt />
                 <h2>Streaming services</h2>
               </div>
               <div class="distribution-block__grid__sub-item">
-                <img src="@/assets/img/icons/icon-server.png" class="icon-server-img" alt="" />
+                <img src="@/assets/img/icons/icon-server.png" class="icon-server-img" alt />
                 <h2>Digital distributors</h2>
               </div>
             </div>
@@ -56,38 +60,60 @@
         </div>
       </div>
     </div>
-  
+
     <div class="content-section content-section--dark">
       <div class="content-section__wrapper">
         <div class="content-block">
-            <div class="content-block__header">
-          
-          </div>
+          <div class="content-block__header"></div>
         </div>
         <div class="text-block text-block--dark">
           <h3 class="text-block__heading">Our partners</h3>
-          <p class="text-block__paragraph text-block__paragraph--center">We distribute to many major partners and streaming services.</p>
+          <p
+            class="text-block__paragraph text-block__paragraph--center"
+          >We distribute to many major partners and streaming services.</p>
           <div class="icon-list icon-list--partners">
-          
             <div class="icon-list__item">
-              <img src="@/assets/img/logos/logo-nextory-white.svg" class="icon-list__icon icon-list__item--large" alt="" />
-            </div>
-            <div class="icon-list__item">
-              <img src="@/assets/img/logos/logo-pubhub.svg" class="icon-list__icon icon-list__item--medium" alt="" />
-            </div>
-              <div class="icon-list__item">
-              <img src="@/assets/img/logos/logo-mofibo-white.svg" class="icon-list__icon icon-list__item--small" alt="" />
+              <img
+                src="@/assets/img/logos/logo-nextory-white.svg"
+                class="icon-list__icon icon-list__item--large"
+                alt
+              />
             </div>
             <div class="icon-list__item">
-              <img src="@/assets/img/logos/logo-storytel-white.svg" class="icon-list__icon icon-list__item--small" alt="" />
+              <img
+                src="@/assets/img/logos/logo-pubhub.svg"
+                class="icon-list__icon icon-list__item--medium"
+                alt
+              />
             </div>
             <div class="icon-list__item">
-              <img src="@/assets/img/logos/logo-bookbeat.svg" class="icon-list__icon icon-list__item--medium" alt="" />
+              <img
+                src="@/assets/img/logos/logo-mofibo-white.svg"
+                class="icon-list__icon icon-list__item--small"
+                alt
+              />
             </div>
             <div class="icon-list__item">
-              <img src="@/assets/img/logos/logo-kirjavalitys.svg" class="icon-list__icon icon-list__item--large" alt="" />
+              <img
+                src="@/assets/img/logos/logo-storytel-white.svg"
+                class="icon-list__icon icon-list__item--small"
+                alt
+              />
             </div>
-         
+            <div class="icon-list__item fix">
+              <img
+                src="@/assets/img/logos/logo-bookbeat.svg"
+                class="icon-list__icon icon-list__item--medium"
+                alt
+              />
+            </div>
+            <div class="icon-list__item">
+              <img
+                src="@/assets/img/logos/logo-kirjavalitys.svg"
+                class="icon-list__icon icon-list__item--large"
+                alt
+              />
+            </div>
           </div>
         </div>
       </div>
@@ -95,26 +121,32 @@
 
     <div class="content-section content-section--white">
       <div class="content-section__wrapper">
-        <h2 class="content-section__heading content-section__heading--center">Print-on-Demand and distribution of CDs</h2>
-        <p class="content-section__text">Digital distribution can be directly handled through our web application and we can easily set up our API to deliver a publication to any additional destinations. We work with all relevant streaming services and/or other sales channels.</p>
-        <p class="content-section__text">If you are interested in our digital distribution services, please contact our distribution team.</p>
+        <h2
+          class="content-section__heading content-section__heading--center"
+        >Print-on-Demand and distribution of CDs</h2>
+        <p
+          class="content-section__text"
+        >Digital distribution can be directly handled through our web application and we can easily set up our API to deliver a publication to any additional destinations. We work with all relevant streaming services and/or other sales channels.</p>
+        <p
+          class="content-section__text"
+        >If you are interested in our digital distribution services, please contact our distribution team.</p>
         <div class="content-section__btn content-section__btn--center">
           <nuxt-link class="btn" to="/about-us">Contact information</nuxt-link>
         </div>
         <div class="distribution-block distribution-block--cd">
           <div class="distribution-block__grid">
             <div class="distribution-block__grid__item distribution-block__grid__item--left">
-              <img src="@/assets/img/icons/icon-cd.png" class="swann-api-img" alt="" />
-                <h2>CD production</h2>
-                <p>CDs are made by our in house robots or with the help of one of our fixed volume production partners and then send to secondary distributors or directly to the relevant sales channel.</p>
+              <img src="@/assets/img/icons/icon-cd.png" class="swann-api-img" alt />
+              <h2>CD production</h2>
+              <p>CDs are made by our in house robots or with the help of one of our fixed volume production partners and then send to secondary distributors or directly to the relevant sales channel.</p>
             </div>
             <div class="distribution-block__grid__item distribution-block__grid__item--right">
               <div class="distribution-block__grid__sub-item">
-                <img src="@/assets/img/icons/icon-server.png" class="icon-server-img" alt="" />
+                <img src="@/assets/img/icons/icon-server.png" class="icon-server-img" alt />
                 <h2>Distributors</h2>
               </div>
               <div class="distribution-block__grid__sub-item">
-                <img src="@/assets/img/icons/icon-server.png" class="icon-server-img" alt="" />
+                <img src="@/assets/img/icons/icon-server.png" class="icon-server-img" alt />
                 <h2>Sales channels</h2>
               </div>
             </div>
@@ -123,33 +155,45 @@
       </div>
     </div>
     <div class="content-section content-section--dark">
-        <div class="content-section__wrapper">
-          <div class="text-block text-block--dark">
-            <h3 class="text-block__heading">Other services</h3>
-            <p class="text-block__paragraph">Swann Studio is ready to assist you with any task, issue or inquiry regarding audiobooks or other spoken audio productions. We also work with ebooks and have a vast general knowledge of the publishing industry. You can see some examples of what we have to offer below. </p>
-            <p class="text-block__paragraph">If you are interested in knowing more about our additional services, feel free to reach out.</p>
-            <div class="content-section__btn content-section__btn--center">
-              <nuxt-link class="btn" to="/about-us">Contact information</nuxt-link>
+      <div class="content-section__wrapper">
+        <div class="text-block text-block--dark">
+          <h3 class="text-block__heading">Other services</h3>
+          <p
+            class="text-block__paragraph"
+          >Swann Studio is ready to assist you with any task, issue or inquiry regarding audiobooks or other spoken audio productions. We also work with ebooks and have a vast general knowledge of the publishing industry. You can see some examples of what we have to offer below.</p>
+          <p
+            class="text-block__paragraph"
+          >If you are interested in knowing more about our additional services, feel free to reach out.</p>
+          <div class="content-section__btn content-section__btn--center">
+            <nuxt-link class="btn" to="/about-us">Contact information</nuxt-link>
+          </div>
+          <div class="icon-list icon-list--distribution">
+            <div class="icon-list__item">
+              <img
+                src="@/assets/img/icons/icon-publishing.svg"
+                class="icon-list__icon icon-list__item--small"
+                alt
+              />
+              <h4>Publishing</h4>
+              <p>We publish a great number of titles ourselves every year. If you are a rights owner or a writer you can always present or submit your material to us. Furthermore our editorial team offers global assistance and consultancy with everything regarding audiobook publication.</p>
             </div>
-            <div class="icon-list icon-list--distribution">
-              <div class="icon-list__item">
-                <img src="@/assets/img/icons/icon-publishing.svg" class="icon-list__icon icon-list__item--small" alt="" />
-                <h4>Publishing</h4>
-                <p>We publish a great number of titles ourselves every year. If you are a rights owner or a writer you can always present or submit your material to us. Furthermore our editorial team offers global assistance and consultancy with everything regarding audiobook publication.</p>
-              </div>
-              <div class="icon-list__item">
-                <img src="@/assets/img/icons/icon-cover-production.svg" class="icon-list__icon icon-list__item--small" alt="" />
-                <h4>Cover production</h4>
-                <p>Our graphics team can produce covers based on existing material or make entirely new ones. We are very aware of the particularities of creating content for the visually competitive streaming market. We can do single projects but also assist with larger series or even entire visual audiobook publication profiles.</p>
-              </div>
-              <div class="icon-list__item icon-list__item--translation">
-                <img src="@/assets/img/icons/icon-translation.svg" class="icon-list__icon" alt="" />
-                <h4>Translation</h4>
-                <p>Our international presence makes us an ideal partner for publications in multiple languages and markets. We are always on hand to assist with any translation services our clients might need.</p>
-              </div>
+            <div class="icon-list__item">
+              <img
+                src="@/assets/img/icons/icon-cover-production.svg"
+                class="icon-list__icon icon-list__item--small"
+                alt
+              />
+              <h4>Cover production</h4>
+              <p>Our graphics team can produce covers based on existing material or make entirely new ones. We are very aware of the particularities of creating content for the visually competitive streaming market. We can do single projects but also assist with larger series or even entire visual audiobook publication profiles.</p>
+            </div>
+            <div class="icon-list__item icon-list__item--translation">
+              <img src="@/assets/img/icons/icon-translation.svg" class="icon-list__icon" alt />
+              <h4>Translation</h4>
+              <p>Our international presence makes us an ideal partner for publications in multiple languages and markets. We are always on hand to assist with any translation services our clients might need.</p>
             </div>
           </div>
         </div>
+      </div>
     </div>
   </section>
 </template>
