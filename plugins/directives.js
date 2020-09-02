027cba1416a98c879debee82e67f02a14a01042d
oskarasg <o.goborovas@yahoo.com>
2020-09-02 12:37:35 +0200

    dependencies updated

--- plugins/directives.js
+++ plugins/directives.js
@@ -1,34 +1,34 @@
-import Vue from 'vue';
-import VMasker from './MaskedInput.js';
-import ValidateJS from 'validate.js';
-import { isEmpty } from 'underscore';
-import VueTelInput from 'vue-tel-input';
-import 'vue-tel-input/dist/vue-tel-input.css';
-import VueYoutube from 'vue-youtube';
+import Vue from "vue"
+import VMasker from "./MaskedInput.js"
+import ValidateJS from "validate.js"
+import { isEmpty } from "underscore"
+import VueTelInput from "vue-tel-input"
+// import "vue-tel-input/dist/vue-tel-input.css"
+import VueYoutube from "vue-youtube"
 
-Vue.use(VueTelInput);
-Vue.use(VueYoutube);
+Vue.use(VueTelInput)
+Vue.use(VueYoutube)
 
-Vue.directive('ui-mask', (el, binding) => {
-    if(binding.value.mask) {
-        VMasker(el).maskPattern(binding.value.mask);
-    }
-});
+Vue.directive("ui-mask", (el, binding) => {
+  if (binding.value.mask) {
+    VMasker(el).maskPattern(binding.value.mask)
+  }
+})
 
 ValidateJS.validators.array = (arrayItems, itemConstraints) => {
-    const arrayItemErrors = arrayItems.reduce((errors, item, index) => {
-      const error = ValidateJS(item, itemConstraints)
-      if (error) errors[index] = { error: error }
-      return errors
-    }, {})
+  const arrayItemErrors = arrayItems.reduce((errors, item, index) => {
+    const error = ValidateJS(item, itemConstraints)
+    if (error) errors[index] = { error: error }
+    return errors
+  }, {})
 
-    return isEmpty(arrayItemErrors) ? null : { errors: arrayItemErrors }
+  return isEmpty(arrayItemErrors) ? null : { errors: arrayItemErrors }
 }
 
 ValidateJS.validators.istrue = function(value, options, key, attributes) {
-    if(value == true) {
-        return null;
-    }
+  if (value == true) {
+    return null
+  }
 
-    return "is not checked";
-};
+  return "is not checked"
+}
