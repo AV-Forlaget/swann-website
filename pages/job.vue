72e6aa59597b079f87395f2de1e25b6d211a8ec3
Klaus Fyhn Jacobsen <kfj@swann-studio.com>
2021-03-18 12:56:02 +0100

    try add job template

---
+++ pages/job.vue
@@ -0,0 +1,13 @@
+<template>
+  <script
+    src="https://recruit.hr-on.com/frame-api/hr.js"
+    type="text/javascript"
+  ></script>
+  <div id="hr-on">
+    <noscript>This page requires javascript<br /></noscript>
+  </div>
+  <script
+    src="https://recruit.hr-on.com/frame-api/customers/swannstudio.js"
+    type="text/javascript"
+  ></script>
+</template>
